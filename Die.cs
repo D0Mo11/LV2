49ec8ef82b8dc686f8079b83f67abf800aefd3d8
D0Mo11 <62610228+D0Mo11@users.noreply.github.com>
2020-04-17 15:48:46 +0200

    Add files via upload

---
+++ Die.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPPOON_LV2
+{
+    class Die
+    {
+        private int numberOfSides;
+        private RandomGenerator randomGenerator;
+
+        public Die(int numberOfSides)
+        {
+            this.numberOfSides = numberOfSides;
+            this.randomGenerator = RandomGenerator.GetInstance();
+        }
+        //public Die(int numberOfSides, Random random)
+        //{
+        //    this.numberOfSides = numberOfSides;
+        //    this.randomGenerator = random;
+        //}
+        public int Roll()
+        {
+            //int rolledNumber = randomGenerator.Next(1, numberOfSides + 1);
+            int rolledNumber = randomGenerator.NextInt(1, numberOfSides + 1);
+            return rolledNumber;
+        }
+        public int NumberOfSides
+        {
+            get { return this.numberOfSides; }
+        }
+    }
+}
---
+++ RandomGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPPOON_LV2
+{
+    class RandomGenerator
+    {
+        private static RandomGenerator instance;
+        private Random random;
+        private RandomGenerator()
+        {
+            this.random = new Random();
+        }
+        public static RandomGenerator GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new RandomGenerator();
+            }
+            return instance;
+        }
+        public int NextInt(int lowerbound, int upperbound)
+        {
+            return random.Next(lowerbound, upperbound);
+        }
+    }
+}
